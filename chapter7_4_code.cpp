a1d6730f5f6f231788a8966b6c37d7b8dba82c9b
mary <41161520+hallelilujah@users.noreply.github.com>
2022-03-11 16:53:42 +0800

    Update chapter7_4_code.cpp

--- chapter7_4_code.cpp
+++ chapter7_4_code.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+long double prob(unsigned num, unsigned cho);
+int main()
+{
+	using namespace std;
+	long double p1,p2;
+	p1=prob(47,5);
+	p2=prob(27,1);
+	cout<<"p1:"<<p1<<"\np2:"<<p2<<"\nthe prob:"<<p1*p2;
+    return 0;
+}
+long double prob(unsigned num, unsigned cho)
+{
+	long double result =1.0;
+	long double n;
+	unsigned p;
+	for(n=num,p=cho;p>0;n--,p--)
+		result=result*p/n;
+	return result;
+}
