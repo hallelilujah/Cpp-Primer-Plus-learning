044f355ddc3d10d3cfc1295c3ac99d560523e2e9
mary <41161520+hallelilujah@users.noreply.github.com>
2022-03-11 17:14:53 +0800

    Update chapter7_5_code.cpp

--- chapter7_5_code.cpp
+++ chapter7_5_code.cpp
@@ -1,1 +1,20 @@
-
+/*此题利用递归函数求解参数阶乘*/
+#include <iostream>
+long product(int);
+int main()
+{
+	using namespace std;
+	int num;
+	while(cin>>num){
+		long result=product(num);
+	cout<<"the "<<num<<"! is: "<<result<<endl;
+	}
+    return 0;
+}
+//注意这个递归函数
+long product(int a)
+{
+	if(a>0)
+	return a*product(a-1);
+	else return 1;
+}
