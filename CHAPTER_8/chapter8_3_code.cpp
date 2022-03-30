4f83d4b5263f2731c47c1f3bef1b47bc26906c9a
mary <41161520+hallelilujah@users.noreply.github.com>
2022-03-30 22:19:44 +0800

    Create chapter8_3_code.cpp

---
+++ CHAPTER_8/chapter8_3_code.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+void change(string & a);
+int main()
+{
+	string a;
+	cout<<"Enter a string(q to quit):";
+	getline(cin,a);
+	while(a!="q"){		
+		change(a);
+		cout<<a<<endl; 
+		cout<<"Next string(q to quit):";
+		getline(cin,a);
+	}
+	cout<<"bye.";
+  return 0;
+}
+void change(string & a)
+{
+	for(int i=0;a[i];i++)
+	{
+		a[i]=toupper(a[i]);
+	}
+ } 
