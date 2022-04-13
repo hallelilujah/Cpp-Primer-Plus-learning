47310f3b494937ca7dd75ca2bfe58b59c58b956d
mary <41161520+hallelilujah@users.noreply.github.com>
2022-04-13 15:58:04 +0800

    Create chapter9_3_code.cpp

---
+++ CHAPTER_9/chapter9_3_code.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstring>
+struct chaff
+{
+	char dross[20];
+	int slag;
+};
+const int BUF=512;
+const int N=2;
+char buffer[BUF];
+
+int main()
+{
+	using namespace std;
+	chaff *pd1, *pd2;
+	pd1=new chaff[N];
+	pd2=new (buffer) chaff[N];
+	
+		strcpy(pd1[0].dross,"aaa");
+		strcpy(pd2[0].dross,"aaa");
+		pd1[0].slag=pd2[0].slag=10;
+		
+		strcpy(pd1[1].dross,"bbb");
+		strcpy(pd2[1].dross,"bbb");
+		pd1[1].slag=pd2[1].slag=20;
+	
+	for(int i=0;i<N;i++)
+	{
+		cout<<pd1[i].dross<<" at "<<&(pd1[i].dross) <<" ; "<<pd1[i].slag<<" at "<<&(pd1[i].slag)<<endl;
+	}
+	for(int i=0;i<N;i++)
+	{
+		cout<<pd2[i].dross<<" at "<<&(pd2[i].dross) <<" ; "<<pd2[i].slag<<" at "<<&(pd2[i].slag)<<endl;
+	}
+	
+	return 0;
+}
